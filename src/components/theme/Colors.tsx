3e7404a52e706b48624bd2ddef5baea16bd3c0d1
Anath <anathonic@gmail.com>
2026-05-21 18:15:47 +0200

    Update Colors.tsx

--- src/components/theme/Colors.tsx
+++ src/components/theme/Colors.tsx
@@ -9,8 +9,8 @@ export const devices = [
   3,
   4,
   5,
-  6,
-  1,
+  // 6,
+  // 1,
 ] as const;
 
 export const getColorsByIndex = (index: number): DeviceColors => {
@@ -20,10 +20,10 @@ export const getColorsByIndex = (index: number): DeviceColors => {
 };
 
 export const Colors: ColorsConfig = {
-  1: { background: "#ffffff", text: "#222324", primary: "#2E2E30", secondary: "#222324" },
-  2: { background: "#222324", text: "#F0E1CD", primary: "#F0E1CD", secondary: "#948685" },
-  3: { background: "#F0E1CD", text: "#93311B", primary: "#871003", secondary:"#93311B" },
-  4: { background: "#871003", text: "#ffffff", primary:  "#ffffff", secondary: "#BDCCE1" },
-  5: { background: "#BDCCE1", text: "#1F2A37", primary:  "#1F2A37", secondary: "#2F5D8C" },
-  6: { background: "#1F2A37", text: "#F9FAFB", primary: "#F9FAFB", secondary: "#BDCCE1" },
+  // 1: { background: "#ffffff", text: "#222324", primary: "#2E2E30", secondary: "#222324" },
+  1: { background: "#222324", text: "#F0E1CD", primary: "#F0E1CD", secondary: "#948685" },
+  2: { background: "#F0E1CD", text: "#93311B", primary: "#871003", secondary:"#93311B" },
+  3: { background: "#871003", text: "#ffffff", primary:  "#ffffff", secondary: "#BDCCE1" },
+  4: { background: "#BDCCE1", text: "#1F2A37", primary:  "#1F2A37", secondary: "#2F5D8C" },
+  // 6: { background: "#1F2A37", text: "#F9FAFB", primary: "#F9FAFB", secondary: "#BDCCE1" },
 };
